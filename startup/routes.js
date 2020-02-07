4ce7cdd9c553f587d8269f210b079e6b9ecfcaf7
Amit Gupta <amit.gupta@daffodilsw.com>
2020-02-07 17:50:53 +0530

    nc

--- startup/routes.js
+++ startup/routes.js
@@ -10,6 +10,11 @@ const fileUpload = require("express-fileupload");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xxs = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 module.exports = app => {
   //dev logging middleware
@@ -19,7 +24,22 @@ module.exports = app => {
   app.use(express.json());
   app.use(cookieParser());
   app.use(fileUpload());
+  //sanitize data
   app.use(mongoSanitize());
+  //security headers
+  app.use(helmet());
+  //prevent cross site scripting
+  app.use(xxs());
+  //rate limiting
+  const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 mins
+    max: 100
+  });
+  app.use(limiter);
+  //prevent http param pollution
+  app.use(hpp());
+  //Enable cors
+  app.use(cors());
   app.use(express.static(path.join(__dirname, "../public")));
   app.use("/api/v1/bootcamps", bootcampRoutes);
   app.use("/api/v1/courses", coursesRoutes);
